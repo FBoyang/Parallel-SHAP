3a2b527638a4d415b81e9857c9966313f853120c
Boyang Fu <boyang19@n2236.hoffman2.idre.ucla.edu>
2021-11-28 16:57:55 -0800

    update

--- simulation.py
+++ simulation.py
@@ -17,7 +17,7 @@ def linear_simulator(X, mcau=30, n=5000, cau=range(30)):
     sigma_g = 0.5
     sigma_eps = 1 - sigma_g
     N = X.shape[0]
-    X_cau = scale(X[:,mcau])
+    X_cau = scale(X[:,0:mcau])
     Kin = X_cau@X_cau.T
     for i in range(3):
         y = np.random.multivariate_normal(np.zeros(N), np.eye(N) * sigma_eps + sigma_g/mcau * Kin)
@@ -30,11 +30,12 @@ def fix_linear_simulator(X, mcau=30, n=5000, cau=range(30)):
     sigma_g = 0.5
     sigma_eps = 1 - sigma_g
     N = X.shape[0]
-    X_cau = scale(X[:,mcau])
+    X_cau = scale(X[:,0:mcau])
     Kin = X_cau@X_cau.T
     
     for i in range(3):
-        beta = np.random.randn(mcau)*sigma_g/mcau
+        beta = (np.random.randn(mcau)*sigma_g/mcau).reshape(-1,mcau)
+        print(X_cau.shape)
         y = X_cau@beta.T
         savepath = f'/u/flashscratch/b/boyang19/CS269/code/Parallel-SHAP/simulations/linear/m{m}/mcau_{mcau}_n_{n}_sim_{i}.pheno'
         dir_creation(savepath)
@@ -47,7 +48,7 @@ def nonlinear_simulator(X, mcau=30, n=5000, cau=range(30)):
     sigma_g = 0.5
     sigma_eps = 1 - sigma_g
     N = X.shape[0]
-    X_cau = scale(X[:,mcau])
+    X_cau = scale(X[:,0:mcau])
     Kactual = pairwise_kernels(X_cau, metric='rbf', gamma=0.1)
     for i in range(3):
         y = np.random.multivariate_normal(np.zeros(N), np.eye(N) * sigma_eps + sigma_g * Kactual)
